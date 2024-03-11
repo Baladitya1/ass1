52a8411cb1346f2e5cc13af808b03bb83602aae2
Baladitya1 <158131420+Baladitya1@users.noreply.github.com>
2024-03-12 00:16:10 +0530

    Q3.py

--- Q3.py
+++ Q3.py
@@ -1,13 +1,19 @@
-def match(A,B):
-    A=set(A)
-    B=set(B)
-    count=0
-    for i in A :
+def match(A, B):
+    A = set(A)
+    B = set(B)
+    
+    count = 0
+    # Iterate through elements in set A
+    for i in A:
+        # Cheking if the element of B is in A
         if i in B:
-            count=count + 1
-    return count
+            count += 1  
+    return count  
 
+# Define two lists
 l1 = [3, 2, 4]
 l2 = [4, 1, 6]
+
+# Call the function
 result = match(l1, l2)
-print(result)
+print(result)  # Output 
